e45e2f7c044e82dd9fffe8caad8746410b6d830d
Mateusz <mateusz.rybczynski@icloud.com>
2025-11-01 07:59:38 +0100

    Initial Commit

---
+++ Vitae/VitaeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitaeApp.swift
+//  Vitae
+//
+//  Created by Mateusz Rybczyński on 01/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct VitaeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
