7f4bdeddc483b9973874298fd338d43aba7d8d31
Mateusz <mateusz.rybczynski@icloud.com>
2025-11-10 22:11:52 +0100

    New translation added

---
+++ Vitae/Components/TextFieldView.swift
@@ -0,0 +1,18 @@
+//
+//  TextField.swift
+//  Vitae
+//
+//  Created by Mateusz Rybczyński on 10/11/2025.
+//
+
+import SwiftUI
+
+struct TextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextField()
+}
